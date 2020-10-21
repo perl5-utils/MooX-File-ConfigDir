05f5c692dba37a881bf773c385c1f74d8cfaae9a
Jens Rehsack <sno@netbsd.org>
2020-10-21 18:24:30 +0200

    lib/MooX/File/ConfigDir.pm: bump copyright year
    Update year of copyright for MooX::File::ConfigDir.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- lib/MooX/File/ConfigDir.pm
+++ lib/MooX/File/ConfigDir.pm
@@ -223,7 +223,7 @@ L<http://search.cpan.org/dist/MooX-File-ConfigDir/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2013-2018 Jens Rehsack.
+Copyright 2013-2020 Jens Rehsack.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
